ac51c7cb56816b77ee2e0c81f1d56e720d4a3cc3
Bernhard Tittelbach <bernhard@tittelbach.org>
2024-03-14 01:32:39 +0100

    make the right NEO mod3 key behave the same with FN pressed or not.

--- chrumm/keymaps/btittelbach_65keys/keymap.c
+++ chrumm/keymaps/btittelbach_65keys/keymap.c
@@ -14,8 +14,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [1] = LAYOUT(
         KC_GRV,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_BSPC, KC_DEL,
-        KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_LPRN, KC_RPRN,
-        _______, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PSLS, XXXXXXX, KC_APP,
+        KC_APP,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_LPRN, KC_RPRN,
+        _______, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PSLS, XXXXXXX, _______,
         _______, KC_NUBS, KC_MRWD, KC_CALC, KC_MFFD, KC_MUTE, KC_NUM,  KC_P0,   KC_PDOT, KC_PENT, KC_PAST, KC_PGUP, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,  KC_HOME, KC_PGDN, KC_END
     )
