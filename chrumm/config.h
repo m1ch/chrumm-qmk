618240d336cc5ac6ce1aeea09f716cf3ad3585de
Bernhard Tittelbach <bernhard@tittelbach.org>
2024-03-21 00:16:48 +0100

    rotate trackpoint by 180° for better mounting

---
+++ chrumm/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define PS2_MOUSE_SCROLL_BTN_MASK (1<<PS2_MOUSE_BTN_MIDDLE) /* Default */
+
+/*
+#define PS2_MOUSE_SCROLL_BTN_SEND 300
+#define PS2_MOUSE_SCROLL_DIVISOR_H 2
+#define PS2_MOUSE_SCROLL_DIVISOR_V 2
+*/
+
+/*
+#define PS2_MOUSE_INVERT_X
+#define PS2_MOUSE_INVERT_X
+*/
+
+#define PS2_MOUSE_ROTATE 180 /* Compensate for South-facing device orientation. */
+
